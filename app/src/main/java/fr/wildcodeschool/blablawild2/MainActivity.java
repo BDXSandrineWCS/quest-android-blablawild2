3beeee6f99b246ef7f3f14e1ad2a1202959dc7c9
sandrine <poupardsandrine@gmail.com>
2018-12-10 16:32:25 +0100

    Ecrire dans la base de données finished

--- app/src/main/java/fr/wildcodeschool/blablawild2/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild2/MainActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -30,5 +37,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        initFirebase();;
+
+    }
+
+    private void initFirebase(){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        myRef.setValue(1);
+        myRef.setValue(false);
+        myRef.setValue(37.2f);
+        myRef.setValue("Welcome into BlablaWild");
+
+        // new Date() crée un objet contenant la date et l'heure du jour
+        ItineraryModel itineraryModel = new ItineraryModel("Toulouse", "Paris", "Eric Cartman", new Date(), 15);
+        DatabaseReference itineraryRef = database.getReference("itinerary");
+        itineraryRef.setValue(itineraryModel);
     }
 }
